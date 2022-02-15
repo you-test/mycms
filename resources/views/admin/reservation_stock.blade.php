0bdf3228370ca5f94c6d3eb1f08368c0019a4682
yusuke <sato11.jp@gmail.com>
2022-02-15 23:30:36 +0900

    finished view of reservation_stock

--- resources/views/admin/reservation_stock.blade.php
+++ resources/views/admin/reservation_stock.blade.php
@@ -1,11 +1,33 @@
 @extends('layouts.admin')
 
 @section('content')
-  <div class="reservation">
+  <div class="reservation-stock">
     <h2>予約在庫管理</h2>
     <form action="" method="POST">
       <label for="month">月選択</label>
-      <input type="month" name="month" id="month">
+      <input type="month" name="month" id="month" class="select-month">
+    </form>
+    <form action="" method="POST"></form>
+      <table>
+        <tr>
+          <th></th>
+          @for ($d = 0; $d < 30; $d++)
+            <th>@php echo $d + 1; @endphp</th>
+          @endfor
+        </tr>
+        <tr>
+          <td class="room-type">シングル</td>
+          @for ($d = 0; $d < 30; $d++)
+            <td><input type="number"></td>
+          @endfor
+        </tr>
+        <tr>
+          <td class="room-type">ダブル</td>
+          @for ($d = 0; $d < 30; $d++)
+            <td><input type="number"></td>
+          @endfor
+        </tr>
+      </table>
     </form>
   </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,7 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <header class="header p-3 d-flex justify-content-between bg-dark">
+    <header class="header p-3 d-flex justify-content-between bg-dark position-fixed w-100">
       <div class="top-title-wrapper">
         <h1 class="mb-3">
           <a href="{{ route('admin.index') }}" class="link-light">ホテルサッポロ予約管理</a>
@@ -37,7 +37,7 @@
       </div>
     </header>
     <div class="main-wrapper d-flex ">
-      <div class="sidebar p-3 bg-dark">
+      <div class="sidebar p-3 bg-dark position-fixed">
         <a href="{{ route('admin.index') }}" class="d-block mb-5 link-light btn menu-btn">ダッシュボード</a>
         <a href="{{ route('facilities.index') }}" class="d-block mb-5 link-light btn menu-btn">施設情報管理</a>
         <a href="{{ route('reservationStock.index') }}" class="d-block mb-5 link-light btn menu-btn">予約在庫管理</a>
